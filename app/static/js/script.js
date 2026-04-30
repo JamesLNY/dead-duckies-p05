4b40a83bec25f0693172efed5503de95656c8583
Sarah Zou <szou0814@gmail.com>
2026-04-29 22:42:20 -0400

    basic farm map scrolling up

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -22,19 +22,16 @@ class Camera {
   //makes scrolling smoother 
   //also i didn't add a control so faster computers will scroll faster
   //not sure if that's a big issue but i can fix it if it's noticeable
+  //James:
+  //WHEN USER CHARACTER IS ADDED, CHANGE THIS TO FOLLOW THE PLAYER
+  //ADD PLAYER CLASS AND MOVE THIS TO PLAYER
+  //tell me when you do this so i can update this func
   move(keys) {
     if (keys['ArrowLeft']) this.x -= this.speed;
     if (keys['ArrowRight']) this.x += this.speed;
     if (keys['ArrowUp']) this.y -= this.speed;
     if (keys['ArrowDown']) this.y += this.speed;
     this.clampEdges();
-  }
-}
-
-class Map {
-  constructor()  {
-    this.width = 1280;
-    this.height = 1040;
   }
 }
 
@@ -52,22 +49,37 @@ class InputHandler {
   }
 }
 
+//doesn't need to be a class, but doing it for organization
 class StarDewValley { 
   constructor(canvas) {
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
     this.map = new Image();
-    this.map.src = '/static/images/farm.png';
     this.map.onload = () => this.render(); 
-    //render func not done yet
+    this.map.src = '/static/images/farm.png';
+    this.camera = new Camera(0, 0, 5); //speed 5 px/frame for now, can adjust later
+    this.input = new InputHandler();
     
-
+    this.loop();
   }
+
+  render() {
+    //comments cuz i will forget how canvas works
+    //clearRect clears the canvas
+    //drawImage takes image, then where in the image to start reading from, how large to read, where on the canvas to draw it, and stretching
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.drawImage(this.map, this.camera.x, this.camera.y, this.canvas.width, this.canvas.height, 0, 0, this.canvas.width, this.canvas.height); 
+  }
+
+  loop() {
+    this.camera.move(this.input.keys);
+    this.render();
+    requestAnimationFrame(() => this.loop());  }
 }
 
 window.addEventListener('load', function() {
   const canvas = document.getElementById('main-canvas');
-  const ctx = canvas.getContext('2d');
   canvas.width = TILE_SIZE * X_RES;
   canvas.height = TILE_SIZE * Y_RES;
+  new StarDewValley(canvas);
 })
