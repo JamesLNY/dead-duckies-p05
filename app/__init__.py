49b3f75258b0cdb6be6c9acc1dbf535c2b85aeff
James Lei <leijames325@gmail.com>
2026-05-01 11:01:42 -0400

    Somehting

--- app/__init__.py
+++ app/__init__.py
@@ -13,5 +13,9 @@ def home_get():
 def game_get():
   return render_template("game.html")
 
+@app.get('/editor')
+def editor_get():
+  return render_template("editor.html")
+
 if __name__ == "__main__":
   app.run(debug=True)
---
+++ app/static/js/editor.js
@@ -0,0 +1,59 @@
+let impassable = new Set();
+const canvas = document.getElementById('main-canvas');
+const ctx = canvas.getContext('2d');
+ctx.imageSmoothingEnabled = false;
+
+const map = new Image();
+map.src = '/static/images/farm.png';
+
+const X = 0;
+const Y = 1;
+const TILE_SIZE = 16;
+
+let paint = true;
+let held = false;
+
+function render() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(map, 0, 0, 1280, 1040, 0, 0, 1280, 1040);
+
+  ctx.fillStyle = 'rgba(255, 0 0, 0.35)';
+  for (const tile of impassable) {
+    let parsed = tile.split(',');
+    ctx.fillRect(parsed[X] * TILE_SIZE, parsed[Y] * TILE_SIZE, 16, 16);
+  }
+}
+
+function draw(e) {
+  render();
+  const tileCol = Math.floor((e.offsetX) / TILE_SIZE);
+  const tileRow = Math.floor((e.offsetY) / TILE_SIZE);
+  if (paint) {
+    impassable.add(`${tileCol}, ${tileRow}`);
+  } else {
+    impassable.delete(`${tileCol}, ${tileRow}`);
+  }
+}
+
+window.addEventListener('load', function() {
+  canvas.width = 1280;
+  canvas.height = 1040;
+  render();
+})
+
+window.addEventListener('keydown', e => {
+  if (e.key === 'p') {
+    console.log(JSON.stringify([...impassable]));
+  } else if (e.key === 't') {
+    add = !add;
+  }
+})
+
+canvas.addEventListener('mousedown', e => {
+  draw(e);
+});
+
+canvas.addEventListener('mousemove', e => {
+  console.log("HERE");
+  draw(e);
+});
