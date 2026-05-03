9c8834b679946c5d4ba9f81ba86b3bfd17656fed
@Kiran1029 <kiranjsoemardjo@gmail.com>
2026-05-02 23:18:38 -0400

    start on time

--- app/static/js/time.js
+++ app/static/js/time.js
@@ -1,16 +1,33 @@
 export default class Time {
   // currData, numTicks, currTime
   constructor() {
-
+    this.currYear = 1
+    this.currDay = 1
+    this.currTime = 600
+    this.numTicks = 0
   }
 
+  getTime() {
+    return currTime
+  }
+
+  nextDay() {
+    currDay++
+    if (currDay == 29) {
+      currYear++
+      currDay = 1
+    }
+  }
   // Overlay on top of upper right of canvas (Only update when time changes)
-  render() {
+  render(ctx) {
 
   }
 
   // called upon each frame load
   update() {
-    
+    numTicks++
+    if (numTicks % 112 == 0) {
+      currTime += 10
+    }
   }
 }
