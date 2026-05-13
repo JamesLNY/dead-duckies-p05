ebb843c79976d3440382f7a1d8fc72bbb5dc4219
emaanas1f <emaana3@nycstudents.net>
2026-05-13 10:56:42 -0400

    change

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -4,7 +4,7 @@ import { MOVEMENT_SPEED, CANVAS_WIDTH, CANVAS_HEIGHT, TILE_IMAGES, TILE_SIZE,
 import Map from './map.js'
 import Player from './player.js';
 import Time from './time.js';
-//import NPC from './npc.js';
+// import NPC from './npc.js';
 import Shop from './shop.js';
 
 class InputHandler {
@@ -64,15 +64,14 @@ class StardewValley {
     this.time = new Time();
 
     //npcs and shops
-    this.pierre = new NPC("Pierre");
-    this.willy = new NPC("Willy");
+    // this.pierre = new NPC("Pierre");
+    // this.willy = new NPC("Willy");
 
     this.pierreShop = new Shop({"seed": 25})
 
     //test
-    this.player.inventory.addItem("wood", 50);
-    this.player.inventory.addItem("stone", 25);
     this.player.inventory.addItem("axe", 1);
+    this.player.inventory.addItem("hoe", 1)
 
     this.player.inventory.renderHotbar(this.uiCtx, this.uiCanvas);
 
